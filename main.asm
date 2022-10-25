b21a92ff115feaf47113d80a7a8db7b96209c378
Daniil Osokin <90867530+eeeeagle@users.noreply.github.com>
2022-10-25 22:52:23 +0400

    .

--- main.asm
+++ main.asm
@@ -2,11 +2,13 @@
 global CMAIN
 
 segment .rodata
-	      A:      dq 5.4
-        B:      dq -8.9
-        C:      dq 0.16
+	A:      dq 1.0
+        B:      dq -4.0
+        C:      dq 4.0
         
-        D:      dq 4.0
+        ZERO:    dq 0.0
+        C_2:    dq 2.0
+        C_4:    dq 4.0
         
 segment .data
         X:      dq 0.0
@@ -30,8 +32,8 @@ CMAIN:
         call TASK_3
         
         mov rsp,rbp
-	      pop rbp
-	      ret
+	pop rbp
+	ret
 
 PRINT_VARS:
         push rbp
@@ -69,7 +71,7 @@ PRINT_VARS:
         
         mov rsp,rbp
       	pop rbp
-	      ret
+	ret
 
 
 
@@ -78,14 +80,18 @@ TASK_1:
         mov rbp, rsp
         sub rsp, 8
         
-        FINIT
-        
         PRINT_STRING "TASK 1"
         NEWLINE
         PRINT_STRING "y = (a*x+b)/c"
         NEWLINE
         NEWLINE
 
+    TASK_1_X87:
+        PRINT_STRING "X87"
+        NEWLINE
+        
+        FINIT
+        
         FLD qword[A]
         FLD qword[X]
         FMUL
@@ -100,13 +106,31 @@ TASK_1:
         NEWLINE
         NEWLINE
         
+    TASK_1_SSE:
+        PRINT_STRING "SSE"
+        NEWLINE
+
+        movsd xmm0, qword[A]
+        movsd xmm1, qword[X]
+        mulsd xmm0, xmm1
+        movsd xmm1, qword[B]
+        addsd xmm0, xmm1
+        movsd xmm1, qword[C]
+        divsd xmm0, xmm1
+        movsd qword[RES], xmm0
+        
+        PRINT_STRING "y = "
+        PRINT_DOUBLE RES
+        NEWLINE
+        NEWLINE
+        
         
         PRINT_STRING "==========="
         NEWLINE
         
         mov rsp,rbp
-	      pop rbp
-	      ret
+	pop rbp
+	ret
 
 
 
@@ -115,25 +139,161 @@ TASK_2:
         mov rbp, rsp
         sub rsp, 8
         
-        PRINT_STRING "TASK 2"
+        PRINT_STRING "TASK 2 SSE"
         NEWLINE
-        PRINT_STRING "a*x^2 + b*x +c = 0"
+        PRINT_STRING "cos(ln(x+a)) = b"
         NEWLINE
         NEWLINE
         
+        call TASK_2_X87
+        call TASK_2_SSE
+                
+        PRINT_STRING "==========="
+        NEWLINE
+        
+        mov rsp,rbp
+	pop rbp
+	ret
+        
+        
+
+TASK_2_SSE:
+        push rbp
+        mov rbp, rsp
+        sub rsp, 8
+        
+        PRINT_STRING "SSE"
+        NEWLINE
+
+        movsd xmm0, qword[A]
+        movsd xmm1, qword[ZERO]
+        comisd xmm0, xmm1
+        je TASK_2_SSE_A
+            
+        movsd xmm0, qword[B]
+        mulsd xmm0, xmm0
+        
+        movsd xmm1, qword[C_4]
+        movsd xmm2, qword[A]
+        mulsd xmm1, xmm2
+        
+        movsd xmm2, qword[C]
+        mulsd xmm1, xmm2
+        
+        subsd xmm0, xmm1
+
+        movsd qword[RES], xmm0
+        
+        PRINT_STRING "D = "
+        PRINT_DOUBLE RES
+        NEWLINE
+        
+        movsd xmm1, qword[ZERO]
+        cmpeqsd xmm0, xmm1
+        je TASK_2_SSE_1_root
+        jb TASK_2_SSE_0_root
+        
+        
+        
+        movsd xmm0, qword[ZERO]
+        movsd xmm1, qword[B]
+        subsd xmm0, xmm1
+        
+        movsd xmm2, qword[RES]
+        sqrtsd xmm1, xmm2
+        
+        addsd xmm0, xmm1
+        
+        movsd xmm1, qword[A]
+        movsd xmm2, qword[C_2]
+        mulsd xmm1, xmm2
+        divsd xmm0, xmm1
+                        
+        movsd qword[TEMP], xmm0
+        
+        PRINT_STRING "x_1 = "
+        PRINT_DOUBLE TEMP
+        NEWLINE
+        
+        
+        
+        movsd xmm0, qword[ZERO]
+        movsd xmm1, qword[B]
+        subsd xmm0, xmm1
+        
+        movsd xmm2, qword[RES]
+        sqrtsd xmm1, xmm2
+        
+        subsd xmm0, xmm1
+        
+        movsd xmm1, qword[A]
+        movsd xmm2, qword[C_2]
+        mulsd xmm1, xmm2
+        divsd xmm0, xmm1
+                        
+        movsd qword[TEMP], xmm0
+        
+        PRINT_STRING "x_2 = "
+        PRINT_DOUBLE TEMP
+        NEWLINE
+                  
+        jmp TASK_2_SSE_EXIT
+    
+    TASK_2_SSE_1_root:        
+        movsd xmm0, qword[ZERO]
+        movsd xmm1, qword[B]
+        subsd xmm0, xmm1
+        
+        movsd xmm2, qword[C_2]
+        movsd xmm1, qword[A]
+        mulsd xmm1, xmm2
+        divsd xmm0, xmm1
+        
+        movsd qword[TEMP], xmm0
+        
+        PRINT_STRING "x = "
+        PRINT_DOUBLE RES
+        NEWLINE
+        
+        jmp TASK_2_X87_EXIT
+        
+    TASK_2_SSE_A:
+        PRINT_STRING "A == 0"
+        NEWLINE
+    TASK_2_SSE_0_root:
+        PRINT_STRING "NO REAL ROOTS"
+        NEWLINE
+        
+    TASK_2_SSE_EXIT:
+        NEWLINE
+        PRINT_STRING "==========="
+        NEWLINE
+    
+        mov rsp,rbp
+	pop rbp
+	ret
+
+TASK_2_X87:
+        push rbp
+        mov rbp, rsp
+        sub rsp, 8
+        
+        
+        PRINT_STRING "X87"
+        NEWLINE
         FINIT
         
         FLD qword[A]
         FLDZ
         FCOMPP
-        je TASK_2_A
+        je TASK_2_X87_A
 
         FINIT
             
         FLD qword[B]
         FLD qword[B]
         FMUL
-        FLD qword[D]
+        FLD qword[C_4]
         FLD qword[A]
         FMUL
         FLD qword[C]
@@ -148,8 +308,8 @@ TASK_2:
         
         FLDZ
         FCOMPP
-        je TASK_2_1_root
-        jb TASK_2_0_root
+        je TASK_2_X87_1_root
+        jb TASK_2_X87_0_root
         
         FINIT
         
@@ -193,9 +353,9 @@ TASK_2:
         PRINT_DOUBLE TEMP
         NEWLINE
                   
-        jmp TASK_2_EXIT
+        jmp TASK_2_X87_EXIT
     
-    TASK_2_1_root:
+    TASK_2_X87_1_root:
         FINIT
         
         FLDZ
@@ -214,23 +374,23 @@ TASK_2:
         PRINT_DOUBLE RES
         NEWLINE
         
-        jmp TASK_2_EXIT
+        jmp TASK_2_X87_EXIT
         
-    TASK_2_A:
+    TASK_2_X87_A:
         PRINT_STRING "A == 0"
         NEWLINE
-    TASK_2_0_root:
+    TASK_2_X87_0_root:
         PRINT_STRING "NO REAL ROOTS"
         NEWLINE
         
-    TASK_2_EXIT:
+    TASK_2_X87_EXIT:
         NEWLINE
         PRINT_STRING "==========="
         NEWLINE
     
         mov rsp,rbp
-	      pop rbp
-	      ret
+	pop rbp
+	ret
 
 
 
@@ -241,7 +401,7 @@ TASK_3:
         
         PRINT_STRING "TASK 3"
         NEWLINE
-        PRINT_STRING "y < log_2(x+a)"
+        PRINT_STRING "y < ln^3(sin(x)+a)"
         NEWLINE
         NEWLINE
         
@@ -261,14 +421,15 @@ TASK_3:
         NEWLINE
         
         FLD qword[Y]
-        FCOMPP
-        jb FALSE
+        
+        FCOMI st1
+        ja TASK_3_FALSE
         
         PRINT_STRING "TRUE"
         NEWLINE
         jmp TASK_3_EXIT
         
-    FALSE:
+    TASK_3_FALSE:
         PRINT_STRING "FALSE"
         NEWLINE
         
@@ -276,6 +437,6 @@ TASK_3:
         PRINT_STRING "==========="
     
         mov rsp,rbp
-	      pop rbp
-	      ret
+	pop rbp
+        ret
         
